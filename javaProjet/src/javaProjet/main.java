f759be69d1d60fe6c02301b1e4fd117d0e8111d4
ananeamani <ananeamani@localhost>
2020-01-25 21:04:25 +0100

    commitit

--- javaProjet/src/javaProjet/main.java
+++ javaProjet/src/javaProjet/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Syste.out.println("Hello Java 15.34.");
+System.out.println("Hello Java 15.34.");
 
 	}
 
